257613800b43ac92c22350e227e3a5de40d9d2c4
cdecu <carlos@decumont.be>
2016-10-19 03:54:15 +0200

    First Commit

--- index.ts
+++ index.ts
@@ -3,71 +3,15 @@
 import {Config} from "./lib/config";
 
 let applog  = require('debug')('mcsc');
-let nconf   = require('nconf');
-let fs      = require('fs');
+let path    = require('path');
 
 // Provide a title to the process in `ps`
+let appRoot = path.resolve(__dirname);
 process.title = 'mcsc';
 
-// program
-//     .version('0.0.1')
-//     .option('-p, --port'     , 'port')
-//     .parse(process.argv);
-//
-console.log(__dirname);
-nconf
-    .argv(
-        {
-        "x": {
-            alias: 'xx',
-            describe: 'Example description for usage generation',
-            demand: true,
-            type:'number'
-            // default: 'some-value'
-        },
-        "y": {
-            alias: 'yy',
-            describe: 'Example description for usage generation',
-            demand: true,
-            // default: 'some-value'
-        }},
-        "Useage fdsfdsfds")
-    .env()
-    .file({ file: '~/.mcsc/config.json' })
-    .load();
+// Load Config
+let te = new Config(applog,appRoot.toString());
+if (te.showHelp)
+    Config.showUsage();
 
-console.log(nconf.version);
-nconf.required(['x', 'y']);
-
-applog('Test3');
-// applog(nconf);
-// applog(nconf.stores.argv);
-// console.log('******************');
-// console.log(nconf.stores.argv.usage);
-console.log('******************');
-console.log(nconf.stores.argv.help());
-applog(nconf.get("x"));
-applog(nconf.get("y"));
-
-nconf.required(['x', 'y']);
-
-
-// console.log(nconf);
-//
-// console.log('Helo');
-//
-// console.log('help: ' + nconf.get('help'));
-//
-// console.log('dbn: ' + nconf.get('dbn'));
-//
-// console.log(nconf.env());
-//
-
-applog('Test1');
-applog('Test2');
-applog('Test3');
-
-
-let te = new Config("ABC");
-te.combineWithAbc("XYZ");
-
+console.log(JSON.stringify(te, null, 6));
--- lib/config.ts
+++ lib/config.ts
@@ -1,16 +1,101 @@
-import {TraceClass} from "typescript-debug";
-
-@TraceClass({ tracePrefix: "mcsc" })
+//To Test  import {TraceClass} from "typescript-debug";
+//To Test  @TraceClass({ tracePrefix: "mcsc" })
 export class Config {
-    constructor(public abc: string) { }
+    public showHelp : boolean = false;
+    public hostname : string;
+    public port     : number;
+    public worldsDir: string;
+    public mcsjars  : string[];
+    
+    constructor(public applog : any , public appRoot : string) {
+        let fs                  = require('fs');
+        let nconf      : any    = Config.nconf();
+        let path                = require('path');
+        let configFile : string = nconf.get('configFile');
+        if (configFile === 'appRoot/config.json')
+            configFile = path.join(appRoot,"/config.json");
+    
+        let os = require("os");
+        this.hostname = os.hostname();
+        this.port     = nconf.get('port');
+        this.worldsDir= nconf.get('worldsDir');
+        this.showHelp = nconf.get('help');
+    
+        this.mcsjars = ['dddd'];
+    
+        applog('Load ' + configFile);
+        try {
+            if (fs.existsSync(configFile)) {
+                let obj = JSON.parse(fs.readFileSync(configFile, 'utf8'));
+                if (typeof obj.port      === "number" ) this.port     = obj.port;
+                if (typeof obj.worldsDir === "string" ) this.worldsDir= obj.worldsDir;
+                if (Array.isArray(obj.mcsjars)        ) this.mcsjars  = obj.mcsjars;
+                applog('.. ' + configFile + ' loaded !');
+            } else {
+                applog('** ' + configFile + ' NOT Found !');
+            }   }
+        catch (ex) {
+            applog(ex.message);
+            }
+        
+        applog('Save ' + configFile);
+        try {
+            let fd = fs.openSync(configFile, 'w');
+            fs.writeSync(fd, JSON.stringify(this, ["port","worldsDir","mcsjars"], 2), 'utf-8');
+            fs.close(fd);
+            applog('.. ' + configFile + ' saved !');
+            }
+        catch (ex) {
+            applog(ex.message);
+        }   }
 
-    public getAbc() {
-        return this.abc;
-    }
 
-    public combineWithAbc(xyz: string) {
-        return this.getAbc() + " and " + xyz;
-    }
+    public static showUsage(){
+        let nconf : any = Config.nconf();
+        console.log(nconf.stores.argv.help());
+        }
+
+    public static nconf():any {
+        let nconf   = require('nconf');
+        nconf
+            .argv({
+                    "p": {
+                        alias: 'port',
+                        describe: 'Http Listening Port',
+                        type:'number',
+                        default: 8080
+                    },
+                    "w": {
+                        alias: 'welcome',
+                        describe: 'Welcome phrase',
+                        default: 'Hello geek'
+                    },
+                    "h": {
+                        alias: 'help',
+                        describe: 'Show this screen',
+                        default: false
+                    },
+                    "d": {
+                        alias: 'worldsDir',
+                        describe: 'Worlds directory',
+                        default: '~/worlds'
+                    },
+                    "c": {
+                        alias: 'configFile',
+                        describe: 'Configuration file',
+                        default: 'appRoot/config.json'
+                    }   },
+                "Minecraft server Controler"
+            )
+            .env({
+                "separator" : '__',
+                "whitelist" :['JAVA_HOME', 'JAVA_ROOT','configFile']
+                })
+            .defaults({
+                "blabla":"blabla"
+                });
+        return nconf;
+        }
 
 }
 
